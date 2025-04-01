0d8d24e17cf2688e4bc64e1db7a46fb918db736f
yhuan165 <144179043+yhuan165@users.noreply.github.com>
2025-04-01 18:03:43 +0000

    Deploying to gh-pages from @ yhuan165/yhuan165.github.io@1f1cd0b7a372a5c350868c2c0548eb59be4b3de5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,12 +9,12 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
+  },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/blog/";
+            window.location.href = "/projects/";
           },
         },{id: "nav-publications",
           title: "publications",
@@ -23,40 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "nav-cat",
-          title: "Cat",
-          description: "This is a page about my cat Tea, short for Medium Thai Iced Milk Tea with 50% Sugar 50% Ice and 2 Boba",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cat/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
@@ -71,6 +43,13 @@ ninja.data = [{
               section: "Dropdown",
               handler: () => {
                 window.location.href = "/blog/";
+              },
+            },{id: "dropdown-cat",
+              title: "cat",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/cat/";
               },
             },{id: "post-a-post-with-plotly-js",
         
