3a30cc0df4033c6fe49c2ab9e6c1a1a735300251
yhuan165 <144179043+yhuan165@users.noreply.github.com>
2025-04-09 20:46:11 +0000

    Deploying to gh-pages from @ yhuan165/yhuan165.github.io@0f4e9a044f93355cdd5df09a2e915967fdccad9a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "My ongoing project in lab.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
